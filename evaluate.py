0ff2f8a045f3da03677627509f7f08572d230654
abzargar <abzargar@ucsc.edu>
2024-02-22 12:00:44 -0800

    evaluate.py modified

--- evaluate.py
+++ evaluate.py
@@ -19,7 +19,7 @@ def evaluate_segmentation(net, valid_iterator, device,criterion,n_valid_examples
     net.eval()
     num_val_batches = len(valid_iterator)
     dice_score = 0
-    mask_list, pred_list = [], []
+    mask_list, pred_list= [], []
     # iterate over the validation set
     with tqdm(total=n_valid_examples, desc='Segmentation Val round', unit='img') as pbar:
         total_val_loss=0
@@ -55,86 +55,30 @@ def evaluate_segmentation(net, valid_iterator, device,criterion,n_valid_examples
                             overlay_img = visualize_segmentation(pred, inp_img=img, overlay_img=True)
                             cv2.imwrite(os.path.join(output_dir,'input_images','images_{:04d}.png'.format(batch_idx*true_masks_copy.shape[0]+i)),visualize_segmentation(mask, inp_img=img, overlay_img=True))
                             cv2.imwrite(os.path.join(output_dir, 'segmentation_predictions', 'images_{:04d}.png'.format(batch_idx*true_masks_copy.shape[0]+ i)),overlay_img)
-
                         pred_list.append(pred)
 
             pbar.update(images.shape[0])
 
     avg_val_loss = total_val_loss / len(valid_iterator)
     if is_avg_prec:
-        avg_list=average_precision(mask_list, pred_list, threshold=prec_thresholds)[0]
-        if output_dir:
-            np.savetxt(os.path.join(output_dir,'precisions.txt'), avg_list, delimiter=',')
-        avg_prec=np.mean(avg_list,axis=0)
-        return dice_score.cpu().numpy() / num_val_batches, avg_prec,avg_val_loss
+        precision_list,recall_list,fscore_list=average_precision_recall_fscore(mask_list, pred_list, threshold=prec_thresholds)
 
-    return dice_score.cpu().numpy() / num_val_batches, None,avg_val_loss
+        scores={
+            'dice_score':dice_score.cpu().numpy() / num_val_batches,
+            'avg_precision':np.mean(precision_list, axis=0),
+            'avg_recall': np.mean(recall_list, axis=0),
+            'avg_fscore': np.mean(fscore_list, axis=0),
+            'avg_val_loss':avg_val_loss
 
+        }
+        return scores
 
-def evaluate_tracker(net, valid_iterator, device,n_valid_examples,is_avg_prec=False,prec_thresholds=[0.5,0.7,0.9],output_dir=None):
-    net.eval()
-    num_val_batches = len(valid_iterator)
-    dice_score = 0
-    mask_list, pred_list= [], []
-    # iterate over the validation set
-    with tqdm(total=n_valid_examples, desc='Tracking Val round', unit='img') as pbar:
-        for batch_idx,batch in enumerate(valid_iterator):
-            images_prev, images_curr,true_masks = batch['image_prev'], batch['image_curr'],batch['mask']
+    scores = {
+        'dice_score': dice_score.cpu().numpy() / num_val_batches,
+        'avg_val_loss': avg_val_loss
+    }
+    return scores
 
-            images_device_prev = images_prev.to(device=device, dtype=torch.float32)
-            images_device_curr = images_curr.to(device=device, dtype=torch.float32)
-            true_masks = true_masks.to(device=device, dtype=torch.long)
-            true_masks = torch.squeeze(true_masks, dim=1)
-            true_masks_copy = copy.deepcopy(true_masks)
-            true_masks = F.one_hot(true_masks, net.module.n_classes).permute(0, 3, 1, 2).float()
-
-            with torch.no_grad():
-                # predict the mask
-                mask_pred = net(images_device_prev,images_device_curr)
-
-                # convert to one-hot format
-                mask_pred_copy = copy.deepcopy(mask_pred.argmax(dim=1))
-                # edge_pred_copy = copy.deepcopy(edge_pred.argmax(dim=1))
-                mask_pred = F.one_hot(mask_pred.argmax(dim=1), net.module.n_classes).permute(0, 3, 1, 2).float()
-                # compute the Dice score, ignoring background
-                dice_score += multiclass_dice_coeff(mask_pred[:, 1:, ...], true_masks[:, 1:, ...],
-                                                        reduce_batch_first=False)
-                if is_avg_prec:
-                    true_masks_copy=true_masks_copy.cpu().numpy()
-                    mask_pred_copy = mask_pred_copy.cpu().numpy()
-                    for i in range(true_masks_copy.shape[0]):
-                        mask,_=ndi.label(remove_small_objects(true_masks_copy[i,:,:]>0,min_size=20,connectivity=1))
-                        mask_list.append(mask)
-                        pred, _ = ndi.label(remove_small_objects(mask_pred_copy[i, :, :]>0,min_size=20,connectivity=1))
-
-                        if output_dir:
-                            img=images_prev[i].cpu().numpy()[0, :, :]
-                            img=(img-np.min(img))/(np.max(img)-np.min(img))*255
-                            overlay_img = visualize_segmentation(pred, inp_img=img, overlay_img=True)
-                            cv2.imwrite(os.path.join(output_dir,'input_tracking_images','images_{:04d}.png'.format(batch_idx*true_masks_copy.shape[0]+i)),img)
-                            cv2.imwrite(os.path.join(output_dir, 'tracking_predictions', 'images_{:04d}.png'.format(batch_idx*true_masks_copy.shape[0]+ i)),overlay_img)
-
-                        pred_list.append(pred)
-
-            pbar.update(images_prev.shape[0])
-
-    if is_avg_prec:
-        avg_list = average_precision(mask_list, pred_list, threshold=prec_thresholds)[0]
-        single_cell_tracking, mitosis_tracking = [], []
-        for i, mask in enumerate(mask_list):
-            if np.max(mask) >1:
-                mitosis_tracking.append(avg_list[i])
-            else:
-                single_cell_tracking.append(avg_list[i])
-
-        if output_dir:
-            np.savetxt(os.path.join(output_dir, 'precisions.txt'), avg_list, delimiter=',')
-        avg_prec = np.mean(avg_list, axis=0)
-        single_cell_avg_prec = np.mean(np.array(single_cell_tracking), axis=0)
-        mitosis_avg_prec = np.mean(np.array(mitosis_tracking), axis=0)
-        return dice_score.cpu().numpy() / num_val_batches, avg_prec, single_cell_avg_prec, mitosis_avg_prec
-
-    return dice_score.cpu().numpy() / num_val_batches, None, None, None
 
 def _label_overlap(x, y):
     """ fast function to get pixel overlaps between masks in x and y
@@ -214,33 +158,8 @@ def _true_positive(iou, th):
 
 
 
-def average_precision(masks_true, masks_pred, threshold=[0.5, 0.75, 0.9]):
-    """ average precision estimation: AP = TP / (TP + FP + FN)
+def average_precision_recall_fscore(masks_true, masks_pred, threshold=[0.5, 0.75, 0.9]):
 
-    This function is based heavily on the *fast* stardist matching functions
-    (https://github.com/mpicbg-csbd/stardist/blob/master/stardist/matching.py)
-
-    Parameters
-    ------------
-
-    masks_true: list of ND-arrays (int) or ND-array (int)
-        where 0=NO masks; 1,2... are mask labels
-    masks_pred: list of ND-arrays (int) or ND-array (int)
-        ND-array (int) where 0=NO masks; 1,2... are mask labels
-
-    Returns
-    ------------
-
-    ap: array [len(masks_true) x len(threshold)]
-        average precision at thresholds
-    tp: array [len(masks_true) x len(threshold)]
-        number of true positives at thresholds
-    fp: array [len(masks_true) x len(threshold)]
-        number of false positives at thresholds
-    fn: array [len(masks_true) x len(threshold)]
-        number of false negatives at thresholds
-
-    """
     not_list = False
     if not isinstance(masks_true, list):
         masks_true = [masks_true]
@@ -248,13 +167,15 @@ def average_precision(masks_true, masks_pred, threshold=[0.5, 0.75, 0.9]):
         not_list = True
     if not isinstance(threshold, list) and not isinstance(threshold, np.ndarray):
         threshold = [threshold]
-    ap = np.zeros((len(masks_true), len(threshold)), np.float32)
+    recall = np.zeros((len(masks_true), len(threshold)), np.float32)
+    precision = np.zeros((len(masks_true), len(threshold)), np.float32)
+    fscore = np.zeros((len(masks_true), len(threshold)), np.float32)
     tp = np.zeros((len(masks_true), len(threshold)), np.float32)
     fp = np.zeros((len(masks_true), len(threshold)), np.float32)
     fn = np.zeros((len(masks_true), len(threshold)), np.float32)
     n_true = np.array(list(map(np.max, masks_true)))
     n_pred = np.array(list(map(np.max, masks_pred)))
-    with tqdm(total=len(masks_true), desc='Precision measurement', unit='img') as pbar:
+    with tqdm(total=len(masks_true), desc='Metrics measurement', unit='img') as pbar:
         for n in range(len(masks_true)):
             if n_pred[n] > 0:
                 iou = _intersection_over_union(masks_true[n], masks_pred[n])[1:, 1:]
@@ -262,8 +183,12 @@ def average_precision(masks_true, masks_pred, threshold=[0.5, 0.75, 0.9]):
                     tp[n, k] = _true_positive(iou, th)
             fp[n] = n_pred[n] - tp[n]
             fn[n] = n_true[n] - tp[n]
-            ap[n] = tp[n] / (tp[n] + fp[n] + fn[n]+1e-6)
+            recall[n] = tp[n] / (tp[n] + fn[n] + 1e-6)
+            precision[n] = tp[n] / (tp[n] + fp[n] + 1e-6)
+            fscore[n] = 2 * (precision[n] * recall[n]) / (precision[n] + recall[n] + 1e-6)
+
             pbar.update(1)
     if not_list:
-        ap, tp, fp, fn = ap[0], tp[0], fp[0], fn[0]
-    return ap, tp, fp, fn
+        precision,recall,fscore, tp, fp, fn = precision[0],recall[0],fscore[0], tp[0], fp[0], fn[0]
+
+    return precision,recall,fscore
